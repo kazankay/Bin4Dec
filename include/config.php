5d18e31d1fbc2cd40cd458a63601348e25616513
Matheus <matheusccontato@gmail.com>
2020-02-26 02:01:35 -0300

    Binário sem função bindec php

--- include/config.php
+++ include/config.php
@@ -1,12 +1,30 @@
 <?php
 
+
 if($_POST['action']){
     $bin = $_POST['binario'];
+    
+    //$dec = bindec($bin) // função que faz a conversão sozinha
 
-    $dec = bindec($bin);
+    $tamanhoDoCampo = strlen($bin);
+    $potencia = array(); 
+    $numero = array();
+    $calculo = array();
+    
+    // recebe o valor da potência e valor de cada campo do binario
+    for ($i=0; $i < $tamanhoDoCampo;  $i++) { 
+        $potencia[] = $tamanhoDoCampo - $i - 1;  
+        $numero[] = substr($bin, ($i * (-1)), 1);
+    }
+    // Armazena os valores de cada potência calculada
+    for ($i=0; $i < $tamanhoDoCampo; $i++) { 
+        $caculo[] = $numero[$i]* pow(2, $potencia[$i]);
+    }
 
-    echo $dec;
+    $dec = array_sum($caculo);
+    
 
+    echo ($dec);
 }
 
 
